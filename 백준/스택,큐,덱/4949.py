4c2ffaf0df907880238d8eac0c95b087bd8d189a
on99yu <dgjjanggu@naver.com>
2023-11-27 12:10:56 +0900

    11-27

---
+++ 백준/스택,큐,덱/4949.py
@@ -0,0 +1,25 @@
+while True:
+    S = input()
+    stack = []
+    if S == '.':
+        break
+    for i in S:
+        if i == '[' or i == '(': 
+            stack.append(i)
+        elif i ==")":
+            if len(stack) != 0 and stack[-1] == '(':
+                stack.pop()
+            else:
+                stack.append(i)
+                break
+        elif i == "]":
+            if len(stack) != 0 and stack[-1] =='[':
+                stack.pop()
+            else: 
+                stack.append(i)
+                break
+            
+    if len(stack) == 0:
+        print('yes')
+    else:
+        print('no')
